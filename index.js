dd3a9e2c74aec8a8bf770d09a1217e6aa3f024d5
nick <nick.flatiron.1@gmail.com>
2022-10-23 12:51:28 -0600

    commit tree b40f91abce2554a1a5fbee61a4d3c5241f948a28

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    }
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+
+function appendCat(name){  
+    let moreCats = [...cats]
+    moreCats.push(name) 
+    return moreCats 
+  }
+
+  function prependCat(name){  
+    let moreCats = [...cats]
+    moreCats.unshift(name) 
+    return moreCats 
+  }
+
+  function removeLastCat(name){  
+    let moreCats = [...cats]
+    moreCats.pop(name) 
+    return moreCats 
+  }
+
+  function removeFirstCat(name){  
+    let moreCats = [...cats]
+    moreCats.shift(name) 
+    return moreCats 
+  }
